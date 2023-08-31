90d44400b11008e1c686a2644079db9b6129f32a
DouriLord <143756147+DouriLord@users.noreply.github.com>
2023-08-31 23:41:25 +0200

    Add files via upload

---
+++ HOMEWOK1.cpp
@@ -0,0 +1,12 @@
+﻿// HOMEWOK1.cpp : Defines the entry point for the application.
+//
+
+#include "HOMEWOK1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "My name is Douri JEONG" << endl;
+	return 0;
+}
